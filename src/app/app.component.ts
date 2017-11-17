25e8103d6edbf173ec313e5034e1ee55c6ab7148
shauryarjain <shauryarjain@gmail.com>
2017-11-17 17:55:29 -0500

    github fullname search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { SearchService } from './search.service';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [SearchService]
 })
 export class AppComponent {
-  title = 'app works!';
+  items: Object;
+  searchTerm$ = new Subject<string>();
+
+  constructor(private searchService: SearchService) {
+    this.searchService.search(this.searchTerm$)
+      .subscribe(items => {
+        this.items = items.items;
+      });
+  }
 }
